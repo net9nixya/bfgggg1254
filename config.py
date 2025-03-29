8b75ff9d901ecbc1555dded44c2d11efc4b9be3c
net9nixya <bshzhfujwv@gmail.com>
2025-03-29 15:24:42 +0500

    +

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
-API_TOKEN = '8141182611:AAEINU5sJ-4ykhX2wnok8WKtEdfGItErpCA'
+API_TOKEN = '7815429800:AAFbxSrHDjV3ONVoIE5rsaYvHDTef03p08Q'
 
 admin = [7305379201, 6465451957, 6400859921]
 start_money = 10000000
@@ -12,4 +12,8 @@ bot_username = 'bfgcopybot'
 chat_log = -1002671526353
 cleaning = 60
 
+# @static
 #8141182611:AAEINU5sJ-4ykhX2wnok8WKtEdfGItErpCA
+
+# @test
+#7815429800:AAFbxSrHDjV3ONVoIE5rsaYvHDTef03p08Q
